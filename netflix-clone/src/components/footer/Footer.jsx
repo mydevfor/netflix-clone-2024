04b3b9661cb4b4617b1ac1559cfad84b89b0969d
birhanu-tech <birhanualemu99@gmail.com>
2024-09-04 11:21:40 +0900

    env

--- netflix-clone/src/components/footer/Footer.jsx
+++ netflix-clone/src/components/footer/Footer.jsx
@@ -47,7 +47,7 @@ function Footer() {
           <div className="service_code">
             <p>Service Code</p>
           </div>
-          <div className="copy-write">&copy; 1997-2024 Netflix, Inc.</div>
+          <div className="copy-write">&copy; 1997-2024 &copy; Netflix, Inc.</div>
         </div>
       </div>
     </>
