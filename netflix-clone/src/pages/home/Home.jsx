b5633aa54244c267a3551a3731945410ba78d539
birhanu-tech <birhanualemu99@gmail.com>
2024-09-05 00:45:52 +0900

    banner

--- netflix-clone/src/pages/home/Home.jsx
+++ netflix-clone/src/pages/home/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import Footer from '../../components/footer/Footer'
 import Header from '../../components/Header/Header'
+import Banner from '../../components/banner/Banner'
 
 function Home() {
   return (
     <>
         <Header/>
+        <Banner/>
         <Footer/>
     </>
   )
